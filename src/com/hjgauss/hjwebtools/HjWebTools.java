2f3efebdbc14ef8361240e6fdee7a0d9fee9e477
HJGauss <repos@opri.it>
2012-12-24 21:14:39 +0100

    start project

---
+++ src/com/hjgauss/hjwebtools/HjWebTools.java
@@ -0,0 +1,13 @@
+package com.hjgauss.hjwebtools;
+
+public class HjWebTools {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("ciao");
+	}
+
+}
