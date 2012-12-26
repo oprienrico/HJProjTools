e9336c1cc8df830ddbaf81f04cd1a7532aea0744
HJGauss <repos@opri.it>
2012-12-26 21:47:01 +0100

    fix script execution, now path indipendent

--- jarbuild/hjwebtools.bat
+++ jarbuild/hjwebtools.bat
@@ -1,3 +1,4 @@
 @echo off
+REM get the script directory with %~dp0
 
-java -jar hjwebtools.jar %1
+java -jar %~dp0hjwebtools.jar %1
--- jarbuild/hjwebtools.sh
+++ jarbuild/hjwebtools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-java -jar hjwebtools.jar $1
+#get get the script directory 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -jar $DIR/hjwebtools.jar $1
